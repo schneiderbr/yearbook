7ca1aab8a8707ae48f781cac78d4d64176e505e3
Jonathan <j.schneider@ecommet.com.br>
2015-05-09 16:14:07 -0300

    credenciais banco

--- util/dataSource.php
+++ util/dataSource.php
@@ -1,11 +1,11 @@
 <?php
  function conn_mysql(){
 
-   $servidor = 'localhost';
+   $servidor = 'br-cdbr-azure-south-a.cloudapp.net';
    $porta = 3306;
-   $banco = "daw_yearbook";
-   $usuario = "daw";
-   $senha = "daw2014";
+   $banco = "appschneiderdb";
+   $usuario = "b5dc8119b63ac3";
+   $senha = "0f047b48";
    
       $conn = new PDO("mysql:host=$servidor;port=$porta;dbname=$banco",$usuario,$senha,array(PDO::ATTR_PERSISTENT => true));
       return $conn;
